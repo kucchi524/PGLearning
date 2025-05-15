a1f3d662b912db0a3cbbec67eaaa654e3d5592e5
kucchi524 <kucchi524@gmail.com>
2025-05-15 23:18:05 +0900

    5章完了

--- python/shomatsu5.py
+++ python/shomatsu5.py
@@ -8,3 +8,26 @@ def is_leapyear():
         print(f'西暦{year}年は、うるう年ではありません。')
 
 is_leapyear()
+
+def int_input(msg):
+    return int(input(f'{msg}を入力してください >>'))
+    
+def calc_payment(amount, people=2):
+    dnum = amount / people
+    pay = dnum // 100 * 100
+    
+    if dnum > pay:
+        pay = int(pay + 100)
+    
+    payorg = amount - pay * (people - 1)
+    
+    return [int(pay), int(payorg)]
+
+def show_payment(pay, payorg, people):
+    print('支払額')
+    print(f'1人あたり{pay}円({people}人)、幹事は{payorg}円です')
+    
+amount = int_input('支払総額')
+people = int_input('参加人数')
+[pay, payorg] = calc_payment(amount, people)
+show_payment(pay, payorg, people)

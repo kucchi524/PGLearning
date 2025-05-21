230c1c3b2d389b1de6a633e6a0df21b663c79d58
kucchi524 <kucchi524@gmail.com>
2025-05-22 01:27:52 +0900

    かんたんJavaScript4章終了

---
+++ JavaScript/かんたんJavaScript/3/js/1.js
@@ -0,0 +1,33 @@
+var x
+y = x + 1
+
+console.log(x)
+console.log(y)
+
+var x = 1
+var y = x + 1
+
+console.log(x)
+console.log(y)
+
+var x = 1
+
+if (true) {
+  let x = 2
+  console.log(x)
+}
+
+console.log(x)
+
+const TAX = 1.08
+
+var pen = 100
+var note = 200
+
+console.log('ペンの税込金額: ' + pen * TAX + '円')
+console.log('ノートの税込金額: ' + note * TAX + '円')
+
+let rensyu = '練習'
+console.log(typeof rensyu)
+
+const PI = 3.14

b47eb12cd6cbc89663a485246731c7039c764d0d
kucchi524 <kucchi524@gmail.com>
2025-05-25 02:44:02 +0900

    かんたんJavaScript3章終了

---
+++ JavaScript/かんたんJavaScript/4/js/1.js
@@ -0,0 +1,37 @@
+var x = 7
+var y = 3
+
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+console.log(x % y)
+
+x = 10
+x += 1
+console.log(x)
+
+x -= 5
+console.log(x)
+
+x *= 2
+console.log(x)
+
+/* 章末問題 */
+let a = 8
+let b = 4
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+
+x = 10
+y = 5
+console.log(x == y)
+console.log(x > y)
+console.log(x < y)
+
+let lastName = '口石'
+let firstName = '凌'
+console.log(lastName + firstName)

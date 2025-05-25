50e6bcd6bc6e1d4e7f7bdab5daffe53584116cae
kucchi524 <kucchi524@gmail.com>
2025-05-25 20:49:04 +0900

    Java入門3章完了

---
+++ Java/Java入門/src/capter3/Int.java
@@ -0,0 +1,40 @@
+package capter3;
+
+public class Int {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(12 + 3);
+		System.out.println(12 - 3);
+		System.out.println(12 * 3);
+		System.out.println(12 / 3);
+		System.out.println(12 % 3);
+		
+		
+		System.out.println(2 + 3 * 4);
+		System.out.println((2 + 3) * 4);
+		
+		System.out.println(1.2 + 0.3);
+		System.out.println(1.2 - 0.3);
+		System.out.println(1.2 * 0.3);
+		System.out.println(1.2 / 0.3);
+		System.out.println(1.2 % 0.3);
+		
+		System.out.println(Math.sqrt(2));
+		System.out.println(Math.sqrt(3));
+		System.out.println(Math.sqrt(5));
+		System.out.println(Math.random() * 6  + 1);
+		System.out.println((int)(Math.random() * 6 + 1));
+		
+		System.out.println((int)'J');
+		System.out.println((int)'a');
+		System.out.println((int)'v');
+		System.out.println((int)'a');
+		
+		System.out.println("total:" + (120+340+560) + "yen");
+		
+		System.out.println("Hello\nJava");
+		System.out.println("\"JVM\" is \"Java Virtual Mchine\"");
+	}
+
+}
---
+++ Java/Java入門/src/capter3/Shomatsu.java
@@ -0,0 +1,37 @@
+package capter3;
+
+public class Shomatsu {
+	
+	public static void main(String[] args) {
+		System.out.println(12 + 3);
+		System.out.println(12 - 3);
+		System.out.println(12 * 3);
+		System.out.println(12 / 3);
+		System.out.println(12 % 3);
+		
+		System.out.println(5 / 2);
+		System.out.println(5 % 2);
+		
+		System.out.println(2 + 3 * 4);
+		System.out.println((2 + 3) * 4);
+		
+		System.out.println(1.2 + 0.3);
+		System.out.println(1.2 - 0.3);
+		System.out.println(1.2 * 0.3);
+		System.out.println(1.2 / 0.3);
+		System.out.println(1.2 % 0.3);
+		
+		System.out.println(Math.sqrt(2));
+		System.out.println(Math.sqrt(3));
+		System.out.println(Math.sqrt(5));
+		
+		System.out.println(Math.random());
+		System.out.println(Math.random() * 6 + 1);
+		
+		System.out.println("total: " + (120 + 340 + 560) + "yen");
+		System.out.println("合計: " + (120 + 340 + 560) + "円");
+		
+		System.out.println("\"JVA\" is \"Java Virtual Mchine\"");
+	}
+
+}

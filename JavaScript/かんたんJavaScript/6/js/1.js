40bccd17235f991ae9e562576a31ecb3ba07c6a5
kucchi524 <kucchi524@gmail.com>
2025-05-28 07:58:53 +0900

    かんたｎJavaScript6章完了

--- JavaScript/かんたんJavaScript/6/js/1.js
+++ JavaScript/かんたんJavaScript/6/js/1.js
@@ -53,3 +53,48 @@ for (let jagged2D of jagged3D) {
     }
   }
 }
+
+var fruit = { apple: 200, orange: 300, banana: 180 }
+console.log(fruit.apple)
+console.log(fruit.orange)
+console.log(fruit.banana)
+
+for (let key in fruit) {
+  console.log(fruit[key])
+}
+
+names.push('小野寺')
+console.log(names[5])
+names.unshift('カビゴン')
+console.log(names[0])
+
+var names1 = ['佐藤', '佐々木']
+var names2 = ['高橋', ...names1, '鈴木', '小松']
+console.log(names2[4])
+
+names.shift()
+console.log(names[0])
+
+names.pop()
+console.log(names[5])
+
+var sports = ['野球', 'バスケットボール', 'サッカー']
+for (let sport of sports) {
+  console.log(sport)
+}
+
+array2D = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 0],
+]
+for (let array1D of array2D) {
+  for (let val of array1D) {
+    console.log(val)
+  }
+}
+
+sports.push('バレーボール')
+console.log(sports[3])
+
+sports.shift(sports)
+console.log(sports[0])
